0eeaa84469a2f7a8d73ae9ba789d3ab28f75bbf9
sachin <jony.malhotra82@gmail.com>
2013-12-13 16:42:47 +0530

    google_auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,7 +39,7 @@ class User < ActiveRecord::Base
     end
 
   def self.find_for_facebook_oauth(auth)
-  user = User.where(:provider => auth.provider, :uid => auth.uid).first
+  user = User.find_by_email(auth.info.email)
   unless user
     user = User.create(name:auth.extra.raw_info.name,
                          provider:auth.provider,
@@ -55,14 +55,14 @@ class User < ActiveRecord::Base
 
 def self.find_for_google_oauth2(access_token)
     data = access_token.info
-    user = User.find_by_provider_and_uid(access_token['provider'], access_token['uid'])
+    user = User.find_by_email(data["email"])
 
     unless user
         user = User.create(name: data["name"],
              email: data["email"],
              uid: access_token.uid,
              provider: access_token.provider,
-             password: Devise.frieTwitterndly_token[0,20]
+             password: Devise.friendly_token[0,20]
             )
     end
     user
